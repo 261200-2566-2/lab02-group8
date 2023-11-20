547b9f34da1dbd157506f4e887a20e6e30bc9e64
Issawara <kmind46@gmail.com>
2023-11-20 17:12:24 +0700

    pm

--- src/AirPurifier.java
+++ src/AirPurifier.java
@@ -1,11 +1,14 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
 
 public class AirPurifier {
     String model = "Pro";
     String serialNo;
     boolean power = false;
     String mode = "auto";
+    private int speedFan = 1;
+    int pm = (int) (Math.random() * 370) ;
     static ArrayList<String> allModel = new ArrayList<>();
     static ArrayList<Integer> numberOfEachModel = new ArrayList<>();
     static int modelCount(){
@@ -25,15 +28,54 @@ public class AirPurifier {
         System.out.println("______________________" );
         System.out.println("  SerialNo : "+serialNo);
         System.out.println("     Model : "+model);
-        if(power == true) System.out.println("     Power : on" );
+        System.out.println("        Pm : "+pm);
+        System.out.println("      PmLv : "+ pmLv());
+        if(power) System.out.println("     Power : on" );
         else System.out.println("     Power : off" );
         System.out.println("      Mode : "+mode );
+        System.out.println("  speedFan : "+speedFan);
         System.out.println("______________________" );
     }
     void turnOn(){ power = true; }
     void turnOff(){ power = false; }
     void toggleOnOff(){ power = !power;}
-    void setMode(String mode){ this.mode = mode;}
+    void setMode(String _mode){
+        if(_mode.equals("auto") || _mode.equals("sleep" ) || _mode.equals("custom") ){
+            this.mode = _mode;
+        }else {
+            System.out.println("This mode does not exist.");
+            return;
+        }
+
+        this.setSpeedFan();
+    }
+
+    String pmLv(){
+        String Lv = "Safe";
+        if(pm<=64) Lv = "Safe";
+        if(pm>64 && pm<=124)  Lv = "Moderate";
+        if(pm>124)  Lv = "Unhealthy";
+        return Lv;
+    }
+    void setSpeedFan(){
+        if(this.mode.equals("sleep")) speedFan = 1;
+        if(this.mode.equals("custom")){
+            Scanner s = new Scanner(System.in);
+            System.out.println("[Custom Mode] Enter the fan speed :");
+            int speed = s.nextInt();
+            if(speed >= 1 && speed <= 3){
+                speedFan = speed;
+            } else{
+                System.out.println("speedFan should be between 1 to 3.");
+                setSpeedFan();
+            }
+        }
+        if(this.mode.equals("auto")){
+            if(pm<=64) speedFan = 1;
+            if(pm>64 && pm<=124) speedFan =2;
+            if(pm>124) speedFan = 3;
+        }
+    }
     AirPurifier(String model,String serialNo){
         this.model = model;
         this.serialNo = serialNo;
@@ -45,6 +87,7 @@ public class AirPurifier {
             numberOfEachModel.set(index,numberOfEachModel.get(index)+1);
         }
         numberOfAPF++;
+        this.setSpeedFan();
     }
     AirPurifier(String serialNo){
         this("Pro",serialNo);
@@ -76,8 +119,10 @@ public class AirPurifier {
         System.out.println(" ");
         System.out.println("a2 mode: "+ a2.mode );
         a2.setMode("sleep");
-        System.out.println("a2 after changeMode: "+ a2.mode );
+        System.out.println("a2 after changeMode(sleep): ");
+        a2.showDisplay();
         a2.setMode("custom");
-        System.out.println("a2 after changeMode: "+ a2.mode );
+        System.out.println("a2 after changeMode(custom): ");
+        a2.showDisplay();
     }
 }
